013cc54e0130fbf01decb6dccf35d590b5b91712
Sheep26 <crabeykiller@gmail.com>
2024-03-16 12:07:21 +1300

    e

--- Flower/flower.gd
+++ Flower/flower.gd
@@ -5,5 +5,8 @@ func _ready():
 	scale = Vector3(1, 1, 1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	scale.y += 0.00009
+func _process(_delta):
+	_grow(0.00009)
+
+func _grow(amount: float):
+	scale.y += amount
