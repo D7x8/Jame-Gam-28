b50bd28b2df1163fdb9e2f8a868378da647024ab
Sheep26 <crabeykiller@gmail.com>
2024-03-16 13:36:15 +1300

    useless ignore please

--- scripts/flower.gd
+++ scripts/flower.gd
@@ -5,6 +5,7 @@ extends StaticBody3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	scale = Vector3(1, 1, 1)
+	head.position = Vector3(0, 1, 0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
